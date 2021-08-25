471e39670b4854a315939093717f88dd9055a7d8
Jayson DeMarchi <jayson.demarchi@gmail.com>
2021-08-24 20:56:22 -0400

    Add environment plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,2 +1,10 @@
+const fs = require('fs-extra');
+const path = require('path');
+
 module.exports = (on, config) => {
+  const configFile = config.env.configFile || 'config/local.json';
+
+  return fs.readJson(
+    path.resolve(configFile)
+  );
 }
