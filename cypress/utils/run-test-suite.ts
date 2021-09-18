0b476e390e5f19c403bad82bc2a160243e3c2e84
Jayson DeMarchi <jayson.demarchi@gmail.com>
2021-09-18 03:41:54 -0400

    (base): Add Fixture Support
    Technically Fixtures were always supported. This just reduces
    the level of overhead a dev will need to manage them.
    
    Hopefully assuming page isolation is enough for fixures, though
    worst case a dev could pull in a fixture from another area.

--- cypress/utils/run-test-suite.ts
+++ cypress/utils/run-test-suite.ts
@@ -2,13 +2,19 @@ export const runTestSuite = (testSuite) => {
   const area = Cypress.config('areas')[testSuite.area];
 
   describe(area.name, () => {
+    let fixture = {};
+    before(() => {
+      cy.fixture(testSuite.area).then((data) => {
+        fixture = data?.fixture;
+      });
+    });
     beforeEach(() => {
-      cy.visit(area.path);
+      cy.visit(fixture?.path || area.path);
     });
 
     testSuite.tests.forEach(test => {
       it(test.name, () => {
-        test.run();
+        test.run(fixture);
       });
     });
   });
