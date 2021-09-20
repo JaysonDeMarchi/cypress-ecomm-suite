981ea93f5cb5e1e3efd03c3d54d89bf39e3571af
Jayson DeMarchi <jayson.demarchi@gmail.com>
2021-09-20 02:29:50 -0400

    (checkout): Add Initial Guest CC Order Test
    Needs a lot of refactoring. This is just to get the ball rolling.

--- cypress/integration/checkout/index.ts
+++ cypress/integration/checkout/index.ts
@@ -1,7 +1,9 @@
 import { runTestSuite } from '../../utils/run-test-suite';
+import { guestShippingCreditCardOrder } from './guest-shipping-credit-card-order';
 
 const area = 'checkout';
 const tests = [
+  guestShippingCreditCardOrder,
 ];
 
 runTestSuite({ area, tests });
