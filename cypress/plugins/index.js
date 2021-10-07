f0f62ad53ddf03954a700c53a4491a91ce8361c6
Jayson DeMarchi <jayson.demarchi@gmail.com>
2021-10-07 18:49:49 -0400

    env (cucumber): Install & Configure Cucumber

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,17 @@
+const browserify = require('@cypress/browserify-preprocessor');
+const cucumber = require('cypress-cucumber-preprocessor').default;
 const fs = require('fs-extra');
 const path = require('path');
+const resolve = require('resolve');
 
 module.exports = (on, config) => {
   const configFile = config.env.configFile || 'config/local.json';
+  const options = {
+    ...browserify.defaultOptions,
+    typescript: resolve.sync('typescript', { baseDir: config.projectRoot }),
+  };
+
+  on('file:preprocessor', cucumber(options));
 
   return fs.readJson(
     path.resolve(configFile)
