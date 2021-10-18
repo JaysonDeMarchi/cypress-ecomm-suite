df740b2af6ec0d92141e1cab783d4b6314fb1f6d
Jayson DeMarchi <jayson.demarchi@gmail.com>
2021-10-18 19:06:35 -0400

    common: Disable Kidcents Redirect

--- cypress/integration/common/given/i-have-kidcents-applied.ts
+++ cypress/integration/common/given/i-have-kidcents-applied.ts
@@ -1,5 +1,5 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
 
 Given(/I have Kidcents applied/, () => {
-  cy.visit('/kidcents/apply');
+  cy.visit('/kidcents/apply?noRedirect=1');
 });
